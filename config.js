a29c9caf62eae5b74f6f6adfc5e4070e1e5dd153
anty-xie <59153942+anty-xie@users.noreply.github.com>
2019-12-24 14:02:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,24 +18,24 @@ var config = {
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
      * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
      * 例如
-     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     * "心爱的小可爱": "./imgs/xiaokeai.png"
      *
      * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "最爱的宝宝": "./imgs/宝宝.jpg",
+        "最爱的宝宝": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "Happy Birthday",
-        play: "来点音乐？",
-        bannar_coming: "灯光起~",
+        turn_on: "一片漆黑……",
+        play: "然后？",
+        bannar_coming: "还有？",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        cake_fadein: "不会这样就完了吧？",
+        light_candle: "接下来？",
+        wish_message: "宝宝生日快乐！",
         story: "A MESSAGE FOR YOU",
     }
 };
